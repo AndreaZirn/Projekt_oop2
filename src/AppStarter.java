f8b5a6eac38268ae9d165eaa829171bcc6f5e226
irinaterribilini <irina.terribilini@students.fhnw.ch>
2015-11-04 10:44:08 +0100

    add classes AppStarter & AppUI

--- src/AppStarter.java
+++ src/AppStarter.java
@@ -1,5 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by andreazirn on 04/11/15.
  */
-public class AppStarter {
+public class AppStarter extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent rootPanel = new AppUI();
+
+        Scene scene = new Scene(rootPanel);
+
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(scene);
+
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(300);
+        primaryStage.centerOnScreen();
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
+
--- src/AppUI.java
+++ src/AppUI.java
@@ -1,5 +1,26 @@
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
 /**
  * Created by andreazirn on 04/11/15.
  */
-public class AppUI {
+public class AppUI extends StackPane {
+
+    private Button button;
+
+    public AppUI() {
+        initializeControls();
+        layoutControls();
+    }
+
+    private void initializeControls() {
+        button = new Button("Hello World");
+    }
+
+    private void layoutControls() {
+        getChildren().add(button);
+    }
+
 }
+
+
